aa775283078bd954efb20aec1aaf47793e5df6ef
Erik Andre <erik.andre@corp.badoo.com>
2015-10-24 12:07:08 +0100

    Removed obsolete pinput, added serial camera tool

--- src/OV7670_control.c
+++ src/OV7670_control.c
@@ -21,18 +21,6 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *   Camera wiring:
- *   3V3	-	3V		;		GND		-	GND
- *   SIOC	-	PB8		;		SIOD	-	PB9
- *   VSYNC  -	PB7		;		HREF	-	PA4
- *   PCLK	-	PA6		;		XCLK	-	PA8
- *   D7		-	PE6		;		D6		-	PE5
- *   D5		-	PB6		;		D4		-	PE4
- *   D3		-	PC9		;		D2		-	PC8
- *   D1		-	PC7		;		D0		-	PC6
- *   RESET	-	/		;		PWDN	-	/
- *
  *	==========================================================================
  */
 
