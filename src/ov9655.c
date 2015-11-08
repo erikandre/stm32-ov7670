e8405042f8c8a2be383e84760cb179caa09dcf29
Erik Andre <erik.andre@corp.badoo.com>
2015-11-08 22:12:05 +0000

    Fixed issue reading regs from ov9655

--- src/ov9655.c
+++ src/ov9655.c
@@ -6,34 +6,63 @@
  */
 
 #define OV9655_DEVICE_ADDRESS    0x60
+//#define OV9655_DEVICE_ADDRESS 0x42
+//#define OV9655_DEVICE_ADDRESS 0x3c
 
 #define OV9655_MIDH       0x1C
 #define OV9655_MIDL       0x1D
 
 #include "ov9655.h"
 
-void OV9655_init() {
+void tryRead(uint8_t addr, uint8_t reg) {
+	Serial_log("Reading from device 0x");
+	Serial_log_hexb(addr);
+	Serial_log(", register 0x");
+	Serial_log_hexb(reg);
+	Serial_log("\r\n");
 	uint8_t status = 0;
 	uint8_t data;
-	data = SCCB_read(OV9655_DEVICE_ADDRESS, 0xff, &status);
+	data = SCCB_read(addr, reg, &status);
 	if (status) {
-		Serial_log("Error ");
+		Serial_log("Error: ");
 		Serial_logi(status);
 		Serial_log("\r\n");
-		return;
-	}
-	Serial_log("h=");
-	Serial_log_hexb(data);
-	Serial_log(",");
-	data = SCCB_read(OV9655_DEVICE_ADDRESS, OV9655_MIDL, &status);
-	if (status) {
-		Serial_log("Error ");
-		Serial_logi(status);
+	} else {
+		Serial_log("Value: 0x");
+		Serial_log_hexb(data);
 		Serial_log("\r\n");
-		return;
 	}
-	Serial_log("l=");
-	Serial_log_hexb(data);
-	Serial_log("\r\n");
+}
+
+void OV9655_init() {
+//	uint8_t i;
+//	for (i = 1; i < 100; i++) {
+//		tryRead(i);
+//	}
+	tryRead(OV9655_DEVICE_ADDRESS, 0x0a); // First read fails (?)
+	tryRead(OV9655_DEVICE_ADDRESS, 0x0a);
+	tryRead(OV9655_DEVICE_ADDRESS, 0x0b);
+//	uint8_t status = 0;
+//	uint8_t data;
+//	data = SCCB_read(OV9655_DEVICE_ADDRESS, 0x0a, &status);
+//	if (status) {
+//		Serial_log("Error ");
+//		Serial_logi(status);
+//		Serial_log("\r\n");
+//		return;
+//	}
+//	Serial_log("h=");
+//	Serial_log_hexb(data);
+//	Serial_log(",");
+//	data = SCCB_read(OV9655_DEVICE_ADDRESS, 0x0b, &status);
+//	if (status) {
+//		Serial_log("Error ");
+//		Serial_logi(status);
+//		Serial_log("\r\n");
+//		return;
+//	}
+//	Serial_log("l=");
+//	Serial_log_hexb(data);
+//	Serial_log("\r\n");
 }
 
--- src/sccb.c
+++ src/sccb.c
@@ -7,165 +7,155 @@
 
 #include "sccb.h"
 
-uint8_t SCCB_waitForIdle() {
-	uint32_t timeout = 0x7FFFFF;
-	while (I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY)) {
-		if ((timeout--) == 0) {
-			return 1;
+#define I2C_SDA_PORT GPIOC
+#define I2C_SDA_PIN  GPIO_Pin_12
+#define I2C_SCL_PORT GPIOB
+#define I2C_SCL_PIN  GPIO_Pin_10
+
+#define SDAH GPIO_SetBits(I2C_SDA_PORT,I2C_SDA_PIN)
+#define SDAL GPIO_ResetBits(I2C_SDA_PORT,I2C_SDA_PIN)
+#define SCLH GPIO_SetBits(I2C_SCL_PORT,I2C_SCL_PIN)
+#define SCLL GPIO_ResetBits(I2C_SCL_PORT,I2C_SCL_PIN)
+#define SDAR GPIO_ReadInputDataBit(I2C_SDA_PORT,I2C_SDA_PIN)
+#define SCLR GPIO_ReadInputDataBit(I2C_SCL_PORT,I2C_SCL_PIN)
+
+void I2C_Delay(void) {
+	unsigned int i = 500;
+	while (i) {
+		i--;
+	}
+}
+
+uint8_t I2C_Start(void) {
+	SDAH;
+	SCLH;
+	I2C_Delay();
+	SDAL;
+	I2C_Delay();
+	SCLL;
+	I2C_Delay();
+	return (0x00);
+}
+
+void I2C_SendByte(unsigned char SendByte) {
+	unsigned char i = 0;
+	for (i = 0; i < 8; i++) {
+		if ((SendByte << i) & 0x80) {
+			SDAH;
+		} else {
+			SDAL;
+		}
+		I2C_Delay();
+		SCLH;
+		I2C_Delay();
+		SCLL;
+		I2C_Delay();
+	}
+}
+
+unsigned char I2C_ReceiveByte(void) {
+	unsigned char i = 8;
+	unsigned char ReceiveByte = 0;
+
+	SDAH;
+	while (i--) {
+		ReceiveByte <<= 1;
+		SCLL;
+		I2C_Delay();
+		SCLH;
+		I2C_Delay();
+		if (SDAR) {
+			ReceiveByte |= 0x01;
 		}
 	}
-	return 0;
+	SCLL;
+	return ReceiveByte;
 }
 
-uint8_t SCCB_start() {
-	I2C_GenerateSTART(I2C2, ENABLE);
-	uint32_t timeout = 0x7FFFFF;
-	while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT)) {
-		if ((timeout--) == 0) {
-			return 1;
-		}
+uint8_t I2C_WaitAck(void) {
+	SDAH;
+	I2C_Delay();
+	SCLH;
+	I2C_Delay();
+	if (SDAR) {
+		return (0xFF);
 	}
-	return 0;
+	SCLL;
+	I2C_Delay();
+	return (0x00);
 }
 
-void SCCB_stop() {
-	I2C_GenerateSTOP(I2C2, ENABLE);
-}
-
-uint8_t SCCB_sendWriteAddress(uint8_t deviceWriteAddr) {
-	I2C_Send7bitAddress(I2C2, deviceWriteAddr, I2C_Direction_Transmitter);
-	uint32_t timeout = 0x7FFFFF;
-	while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
-		if ((timeout--) == 0) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-uint8_t SCCB_writeByte(uint8_t value) {
-	I2C_SendData(I2C2, value);
-	uint32_t timeout = 0x7FFFFF;
-	while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
-		if ((timeout--) == 0) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-uint8_t SCCB_readByte() {
-	uint32_t timeout = 0x7FFFFF;
-	while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_RECEIVED)) {
-		if ((timeout--) == 0) {
-			return 0xff;
-		}
-	}
-	return I2C_ReceiveData(I2C2);
-}
-
-uint8_t SCCB_write(uint8_t deviceWriteAddr, uint8_t regAddr, uint8_t data) {
-	// Wait for idle before we can start
-	if (SCCB_waitForIdle()) {
-		return SCCB_ERROR_BUSY;
-	}
-	// Send the start bit to initiate the transfer
-	if (SCCB_start()) {
-		return SCCB_ERROR_START_FAIL;
-	}
-	// Send the slave write address
-	if (SCCB_sendWriteAddress(deviceWriteAddr)) {
-		return SCCB_ERROR_ADDR_FAIL;
-	}
-	// Send the address of the register we are updating
-	if (SCCB_writeByte(regAddr)) {
-		return SCCB_ERROR_DATA_FAIL;
-	}
-	// Send the updated register value
-	if (SCCB_writeByte(data)) {
-		return SCCB_ERROR_DATA_FAIL;
-	}
-	// End by sending the stop bit
-	SCCB_stop();
-	return 0;
+void I2C_Stop(void) {
+	SDAL;
+	I2C_Delay();
+	SCLH;
+	I2C_Delay();
+	SDAH;
+	I2C_Delay();
 }
 
 uint8_t SCCB_read(uint8_t deviceWriteAddr, uint8_t regAddr, uint8_t* status) {
-	// Wait for idle before we can start
-	if (SCCB_waitForIdle()) {
-		*status = SCCB_ERROR_BUSY;
-		return 0;
-	}
-	// Send the start bit to initiate the transfer
-	if (SCCB_start()) {
+	unsigned char Recieved_Data = 0;
+	I2C_Delay();
+	I2C_Start();
+	I2C_SendByte(deviceWriteAddr);
+	if (I2C_WaitAck()) {
 		*status = SCCB_ERROR_START_FAIL;
-		return 0;
+		return (0xFF);
 	}
-	// Send the slave (write) address
-	if (SCCB_sendWriteAddress(deviceWriteAddr)) {
+	I2C_SendByte(regAddr);
+	if (I2C_WaitAck()) {
 		*status = SCCB_ERROR_ADDR_FAIL;
-		return 0;
+		return (0xFF);
 	}
-	// Send the address of the register we are updating
-	if (SCCB_writeByte(regAddr)) {
-		*status = SCCB_ERROR_DATA_FAIL;
-		return 0;
-	}
-	// Send stop before we start reading the registry value
-	SCCB_stop();
-	// Send the start bit to start the read
-	if (SCCB_start()) {
-		*status = SCCB_ERROR_START_FAIL;
-		return 0;
-	}
-	// Send the slave (read) address
-	if (SCCB_sendWriteAddress(deviceWriteAddr)) {
+	I2C_Stop();
+	I2C_Start();
+	I2C_SendByte(deviceWriteAddr + 1);
+	if (I2C_WaitAck()) {
 		*status = SCCB_ERROR_ADDR_FAIL;
-		return 0;
+		return (0xFF);
 	}
-	uint8_t data = SCCB_readByte();
-	// Send stop again to finish the transfer
-	SCCB_stop();
-	return data;
+	Recieved_Data = I2C_ReceiveByte();
+	I2C_Stop();
+	return Recieved_Data;
 }
 
 void SCCB_init(void) {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	I2C_InitTypeDef I2C_InitStructure;
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	/* Enable GPIOB clocks */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-	// GPIO config
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	/*SDA Pin Configuration*/
+	GPIO_InitStructure.GPIO_Pin = I2C_SDA_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_Init(I2C_SDA_PORT, &GPIO_InitStructure);
+	/*SCL Pin Configuration*/
+	GPIO_InitStructure.GPIO_Pin = I2C_SCL_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(I2C_SCL_PORT, &GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
+//	// GPIO config
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+//	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+//	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+//	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+//	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+//	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-	// GPIO AF config
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_I2C2);
-
-	// I2C config
-	I2C_DeInit(I2C2);
-	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
-	I2C_InitStructure.I2C_OwnAddress1 = 0x00;
-	I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
-	I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-	I2C_InitStructure.I2C_ClockSpeed = 100000;
-	I2C_ITConfig(I2C2, I2C_IT_ERR, ENABLE);
-	I2C_Init(I2C2, &I2C_InitStructure);
-	I2C_Cmd(I2C2, ENABLE);
 }
 
